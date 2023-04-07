95b1ddf3590f06ff73f8210d9fce9119fc8d19f5
batuhan topcu <topcubatuhan00@gmail.com>
2023-04-08 00:57:01 +0300

    Add project files.

---
+++ OnlineMuhasebeServer.Presentation/AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace OnlineMuhasebeServer.Presentation
+{
+    public class AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(Assembly).Assembly;
+    }
+}
