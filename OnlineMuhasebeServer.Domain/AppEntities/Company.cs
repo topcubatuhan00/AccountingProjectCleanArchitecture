ff24e1de3b0df34975db4556086dbdd0e1c25624
batuhan topcu <topcubatuhan00@gmail.com>
2023-04-08 01:45:26 +0300

    first tables created with code first

---
+++ OnlineMuhasebeServer.Domain/AppEntities/Company.cs
@@ -0,0 +1,20 @@
+﻿using OnlineMuhasebeServer.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMuhasebeServer.Domain.AppEntities
+{
+    public sealed class Company : Entity
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string IdentityNumber { get; set; }
+        public string TaxDepartment { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
